b446ed4287843e1a692149fa306a6609cf67ca92
Francisco Sepúlveda <fsepulveda@amf.cl>
2023-10-21 03:32:20 -0300

    merge

--- BackSecurity.Dto/Account/ChangePassResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace BackSecurity.Dto.Account
-{
-    public class ChangePassResponse
-    {
-        public bool Succed { get; set; }
-
-        public string Description { get; set; }
-    }
-}
