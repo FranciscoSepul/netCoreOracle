0943e60a07c4046ade1893989a21f696ed8ed131
Francisco Sepúlveda <fsepulveda@amf.cl>
2023-11-04 04:22:58 -0300

    se crea endpoint meses

---
+++ BackSecurity.Dto/Meses/Meses.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackSecurity.Dto.Meses
+{
+    public class Meses
+    { 
+        public int Id { get; set; }
+        public string Mes { get; set; }
+    }
+
+
+}
--- BackSegurity/Controllers/DireccionController.cs
+++ BackSegurity/Controllers/DireccionController.cs
@@ -14,6 +14,7 @@ using BackSecurity.Controllers.Common;
 using BackSecurity.Dto.Authentication;
 using BackSecurity.Constants.Constants;
 using BackSecurity.Dto.Company;
+using BackSecurity.Dto.Meses;
 
 namespace BackSecurity.Controllers
 {
@@ -52,7 +53,62 @@ namespace BackSecurity.Controllers
             List<Dto.Region.Item> regions = _DireccionService.DireccionList();
             return Ok(regions);
         }
-
+        [HttpGet("GetAllMeses")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetAllMeses()
+        {
+            List<Meses> meses =new();
+            
+            Meses meses1 = new();
+            meses1.Mes="Enero";
+            meses1.Id=1;
+            meses.Add(meses1);
+            Meses meses2 = new();
+            meses2.Mes="Febrero";
+            meses2.Id=2;
+            meses.Add(meses2);
+            Meses meses3 = new();
+            meses3.Mes="Marzo";
+            meses3.Id=3;
+            meses.Add(meses3);
+            Meses meses4 = new();
+            meses4.Mes="Abril";
+            meses4.Id=4;
+            meses.Add(meses4);
+            Meses meses5 = new();
+            meses5.Mes="Mayo";
+            meses5.Id=5;
+            meses.Add(meses5);
+            Meses meses6 = new();
+            meses6.Mes="Junio";
+            meses6.Id=6;
+            meses.Add(meses6);
+            Meses meses7 = new();
+            meses7.Mes="Julio";
+            meses7.Id=7;
+            meses.Add(meses7);
+            Meses meses8 = new();
+            meses8.Mes="Agosto";
+            meses8.Id=8;
+            meses.Add(meses8);
+            Meses meses9 = new();
+            meses9.Mes="Septiembre";
+            meses9.Id=9;
+            meses.Add(meses9);
+            Meses meses10 = new();
+            meses10.Mes="Octubre";
+            meses10.Id=10;
+            meses.Add(meses10);
+            Meses meses11 = new();
+            meses11.Mes="Noviembre";
+            meses11.Id=11;
+            meses.Add(meses11);
+            Meses meses12 = new();
+            meses12.Mes="Diciembre";
+            meses12.Id=12;
+            meses.Add(meses12);
+            return Ok(meses);
+        }
 
     }
 }
